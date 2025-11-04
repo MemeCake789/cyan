f77c1aad9c865089251a0760231df8fdb3d17304
memecake788 <memecake788@gmail.com>
2025-11-04 20:56:45 +0000

    Vite Fix

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 import { URL } from 'url';
-import { viteObfuscateFile } from 'vite-plugin-obfuscator'
 
 // https://vite.dev/config/
 export default defineConfig(({ command }) => ({
@@ -35,32 +34,7 @@ export default defineConfig(({ command }) => ({
           }
         });
       }
-    },
-    command === 'build' && viteObfuscateFile({
-      compact: true,
-      controlFlowFlattening: true,
-      controlFlowFlatteningThreshold: 0.75,
-      deadCodeInjection: true,
-      deadCodeInjectionThreshold: 0.4,
-      debugProtection: false,
-      debugProtectionInterval: 0,
-      disableConsoleOutput: true,
-      identifierNamesGenerator: 'hexadecimal',
-      log: false,
-      numbersToExpressions: true,
-      renameGlobals: false,
-      rotateStringArray: true,
-      selfDefending: true,
-      shuffleStringArray: true,
-      splitStrings: true,
-      splitStringsChunkLength: 10,
-      stringArray: true,
-      stringArrayEncoding: ['base64'],
-      stringArrayThreshold: 0.75,
-      target: 'browser',
-      transformObjectKeys: true,
-      unicodeEscapeSequence: false
-    })
+    }
   ],
   server: {
     proxy: {
